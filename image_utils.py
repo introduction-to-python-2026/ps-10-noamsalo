3167b27fd792b9f3e20dfa3b03fa70e84a65183d
noamsalo <noamsal30@gmail.com>
2026-01-07 17:59:28 +0200

    Refactor image_utils.py for edge detection
    Refactored image processing functions and removed unused code.

--- image_utils.py
+++ image_utils.py
@@ -1,50 +1,17 @@
-import importlib
-import image_utils
-importlib.reload(image_utils)
-
-from image_utils import load_image
-
-image_path = '/content/Sandy.jpg'
-loaded_image = load_image(image_path)
-
-import matplotlib.pyplot as plt
-from image_utils import load_image, edge_detection
-
-from skimage.filters import median
-from skimage.morphology import ball
-
-clean_image = median(loaded_image, ball(1))
-
-clean_image = edge_detection(clean_image)
-clean_image = np.rot90(clean_image, k=-1)
-
-plt.figure(figsize=(5,5))
-plt.imshow(clean_image, cmap = "gray")
-plt.axis('off')
-plt.show()
-
-plt.figure(figsize=(3,3))
-plt.hist(clean_image.flatten(), bins = 256)
-
-binary_clean_image = np.where(clean_image < 50,0,1)
-binary_clean_image = np.rot90(binary_clean_image, k=0)
-
-plt.figure(figsize=(5,5))
-plt.imshow(binary_clean_image, cmap = "gray")
-
 from PIL import Image
+import numpy as np
+from scipy.signal import convolve2d
 
-edge_image = Image.fromarray(edge_binary)
-edge_image.save('my_edges.png')
+def load_image(path):
+ image = Image.open(path)
+ image_array = np.array(image)
+ return image_array
 
-from PIL import Image
-import matplotlib.pyplot as plt
-
-loaded_edge_image = Image.open('my_edges.png')
-
-loaded_edge_image = np.rot90(loaded_edge_image, k=-1)
-plt.figure(figsize=(10, 10))
-plt.imshow(loaded_edge_image, cmap='gray')
-plt.axis('off')
-plt.show()
-
+def edge_detection(image):
+ gray_image = np.mean(image, axis = 2)
+ kernelX = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+ kernelY = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+ edgeX = convolve2d(gray_image, kernelX, mode = "same")
+ edgeY = convolve2d(gray_image, kernelY, mode = "same")
+ edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+ return edgeMAG
