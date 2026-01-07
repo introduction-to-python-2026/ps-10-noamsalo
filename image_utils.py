0fbf84da7dcc2ace86aaa17270147feb90b63b3b
noamsalo <noamsal30@gmail.com>
2026-01-07 17:58:20 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,49 +1,50 @@
-import numpy as np
+import importlib
+import image_utils
+importlib.reload(image_utils)
+
+from image_utils import load_image
+
+image_path = '/content/Sandy.jpg'
+loaded_image = load_image(image_path)
+
 import matplotlib.pyplot as plt
-from scipy.signal import convolve2d
+from image_utils import load_image, edge_detection
 
-def load_image(path):
-    """
-    טוענת תמונה ומחזירה אותה כמערך numpy.
-    הוראה מס' 1 [cite: 2]
-    """
-    # קריאת התמונה והחזרה כמערך
-    img = plt.imread(path)
-    return np.array(img)
+from skimage.filters import median
+from skimage.morphology import ball
 
-def edge_detection(img_array):
-    """
-    מבצעת זיהוי קצוות על התמונה.
-    הוראה מס' 2 [cite: 3]
-    """
-    # 1. המרה לאפור (Grayscale) על ידי מיצוע 3 הערוצים
-    # הוראות 
-    # שים לב: אנחנו משתמשים ב-axis=2 כדי למצע את ערוצי הצבע (RGB)
-    gray_img = np.mean(img_array, axis=2)
+clean_image = median(loaded_image, ball(1))
 
-    # 2. הגדרת הפילטרים (Kernels)
-    # הוראות [cite: 8, 9, 10]
-    kernelY = np.array([
-        [1, 2, 1],
-        [0, 0, 0],
-        [-1, -2, -1]
-    ])
-    
-    kernelX = np.array([
-        [1, 0, -1],
-        [2, 0, -2],
-        [1, 0, -1]
-    ])
+clean_image = edge_detection(clean_image)
+clean_image = np.rot90(clean_image, k=-1)
 
-    # 3. ביצוע קונבולוציה (Convolution)
-    # הוראות [cite: 12, 13, 14, 15]
-    # mode='same' -> שומר על גודל התמונה המקורי
-    # boundary='fill', fillvalue=0 -> ריפוד באפסים (padding=0)
-    edgeY = convolve2d(gray_img, kernelY, mode='same', boundary='fill', fillvalue=0)
-    edgeX = convolve2d(gray_img, kernelX, mode='same', boundary='fill', fillvalue=0)
+plt.figure(figsize=(5,5))
+plt.imshow(clean_image, cmap = "gray")
+plt.axis('off')
+plt.show()
 
-    # 4. חישוב המגניטודה (העוצמה הכוללת)
-    # הוראה [cite: 18, 19]
-    edgeMAG = np.sqrt(np.square(edgeX) + np.square(edgeY))
-    
-    return edgeMAG
+plt.figure(figsize=(3,3))
+plt.hist(clean_image.flatten(), bins = 256)
+
+binary_clean_image = np.where(clean_image < 50,0,1)
+binary_clean_image = np.rot90(binary_clean_image, k=0)
+
+plt.figure(figsize=(5,5))
+plt.imshow(binary_clean_image, cmap = "gray")
+
+from PIL import Image
+
+edge_image = Image.fromarray(edge_binary)
+edge_image.save('my_edges.png')
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+loaded_edge_image = Image.open('my_edges.png')
+
+loaded_edge_image = np.rot90(loaded_edge_image, k=-1)
+plt.figure(figsize=(10, 10))
+plt.imshow(loaded_edge_image, cmap='gray')
+plt.axis('off')
+plt.show()
+
